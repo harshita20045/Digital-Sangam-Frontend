ff6e4be0da389ec0459ec0f0808de28701fa3080
Harshita <baghelharshita2005@gmail.com>
2025-08-29 21:42:38 +0530

    google added

--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import EndPoint from "../../apis/EndPoint";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 import { Link, useNavigate } from "react-router-dom";
 
 function SignUp() {
@@ -41,14 +41,13 @@ function SignUp() {
         password: "",
         contact: "",
       });
-      setIsLoading(false)
+      setIsLoading(false);
     } catch (err) {
       console.log(err);
     }
   };
 
   return (
-    
     <div className="container d-flex justify-content-center mt-5 align-items-center vh-100">
       <button
         className="btn btn-link position-absolute"
@@ -64,109 +63,131 @@ function SignUp() {
         &larr; Back
       </button>
 
-      {isLoading?(<CircularProgress />):(
-      <div className="col-md-5 col-sm-10 col-12 rounded-4">
-       
-        <div className="text-center mb-3">
-          <div
-            style={{
-              height: "60px",
-              width: "60px",
-              background: "linear-gradient(to right, #f97316, #dc2626)",
-              color: "white",
-              fontSize: "32px",
-              fontWeight: "600",
-              borderRadius: "20px",
-              display: "inline-flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            DS
-          </div>
-          <h3 className="mt-3 fw-bold">Welcome to DigitalSangam</h3>
-          <p className="text-muted small">
-            Join our cultural heritage community
-          </p>
-        </div>
-        {verify ? (
-          <form
-            className="py-5 px-5"
-            onSubmit={handleSubmit}
-            style={{ border: "1px solid #d7d6d6ff", borderRadius: "30px" }}
-          >
-            <h4 className="text-center mb-3">Create Account</h4>
-            <div className="mb-3">
-              <label className="form-label fw-semibold">Name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your name"
-                onChange={(event) =>
-                  setState({ ...state, name: event.target.value })
-                }
-              />{" "}
-            </div>{" "}
-            <div className="mb-3">
-              <label className="form-label fw-semibold">Email</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Enter your email"
-                onChange={(event) =>
-                  setState({ ...state, email: event.target.value })
-                }
-              />{" "}
-            </div>{" "}
-            <div className="mb-3">
-              <label className="form-label fw-semibold">Phone Number</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your phone number"
-                onChange={(event) =>
-                  setState({ ...state, contact: event.target.value })
-                }
-              />{" "}
-            </div>
-            <div className="mb-3">
-              <label className="form-label fw-semibold">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Enter your password"
-                onChange={(event) =>
-                  setState({ ...state, password: event.target.value })
-                }
-              />
-            </div>
-            <Link className="my-2 text-center" to="/login" style={{color:"black"}}>Do You have Account Already ?</Link>
-            <div className="d-flex justify-content-between align-items-center mb-3">
-              <div>
-                <input type="checkbox" id="remember" />
-                <label htmlFor="remember" className="ms-1 small">
-                  I agree to the Terms of Service and Privacy Policy
-                </label>
-              </div>
-            </div>
-            <button
-              type="submit"
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <div className="col-md-5 col-sm-10 col-12 rounded-4">
+          <div className="text-center mb-3">
+            <div
               style={{
+                height: "60px",
+                width: "60px",
                 background: "linear-gradient(to right, #f97316, #dc2626)",
+                color: "white",
+                fontSize: "32px",
+                fontWeight: "600",
+                borderRadius: "20px",
+                display: "inline-flex",
+                alignItems: "center",
+                justifyContent: "center",
               }}
-              className="btn btn-primary w-100"
             >
-              Sign up
-            </button>
-          </form>
-        ) : (
-          <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h2>✅ Please verify your email</h2>
-            <p>We’ve sent a verification link to your email address.</p>
-            <p>Redirecting you to login page...</p>
+              DS
+            </div>
+            <h3 className="mt-3 fw-bold">Welcome to DigitalSangam</h3>
+            <p className="text-muted small">
+              Join our cultural heritage community
+            </p>
           </div>
-        )}
-      </div>)}
+          {verify ? (
+            <form
+              className="py-5 px-5"
+              onSubmit={handleSubmit}
+              style={{ border: "1px solid #d7d6d6ff", borderRadius: "30px" }}
+            >
+              <h4 className="text-center mb-3">Create Account</h4>
+              <div className="mb-3">
+                <label className="form-label fw-semibold">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your name"
+                  onChange={(event) =>
+                    setState({ ...state, name: event.target.value })
+                  }
+                />{" "}
+              </div>{" "}
+              <div className="mb-3">
+                <label className="form-label fw-semibold">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter your email"
+                  onChange={(event) =>
+                    setState({ ...state, email: event.target.value })
+                  }
+                />{" "}
+              </div>{" "}
+              <div className="mb-3">
+                <label className="form-label fw-semibold">Phone Number</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your phone number"
+                  onChange={(event) =>
+                    setState({ ...state, contact: event.target.value })
+                  }
+                />{" "}
+              </div>
+              <div className="mb-3">
+                <label className="form-label fw-semibold">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter your password"
+                  onChange={(event) =>
+                    setState({ ...state, password: event.target.value })
+                  }
+                />
+              </div>
+              <Link
+                className="my-2 text-center"
+                to="/login"
+                style={{ color: "black" }}
+              >
+                Do You have Account Already ?
+              </Link>
+              <div className="d-flex justify-content-between align-items-center mb-3">
+                <div>
+                  <input type="checkbox" id="remember" />
+                  <label htmlFor="remember" className="ms-1 small">
+                    I agree to the Terms of Service and Privacy Policy
+                  </label>
+                </div>
+              </div>
+              <button
+                type="submit"
+                style={{
+                  background: "linear-gradient(to right, #f97316, #dc2626)",
+                }}
+                className="btn btn-primary w-100"
+              >
+                Sign up
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-dark w-100 mt-3"
+                onClick={() =>
+                  (window.location.href = "https://digital-sangam-backend.onrender.com/auth/google")
+                }
+              >
+                <img
+                  src="https://developers.google.com/identity/images/g-logo.png"
+                  alt="google logo"
+                  style={{ width: "20px", marginRight: "10px" }}
+                />
+                Sign up with Google
+              </button>
+            </form>
+          ) : (
+            <div style={{ textAlign: "center", marginTop: "50px" }}>
+              <h2>✅ Please verify your email</h2>
+              <p>We’ve sent a verification link to your email address.</p>
+              <p>Redirecting you to login page...</p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
